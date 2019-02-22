2205f9f1f49ac1639e12987f9c86684840fa5a76
herkyl <s.herkyl@gmail.com>
2019-02-22 18:55:16 +0200

    Add more comments

--- static/script.js
+++ static/script.js
@@ -1,10 +1,16 @@
 // 👇 PS! Replace this with your own channel ID 🚨
 const CLIENT_ID = 'YOUR_SCALEDRONE_ID';
 
+// public room
 const PUBLIC_ROOM_NAME = 'observable-room';
+// array of connected memebers
 let members = [];
+// the session user
 let me;
+// keeping track of which room the user has selected
 let selectedRoom = PUBLIC_ROOM_NAME;
+// room name to messages map, this is used to store messages for displaying them
+// at a later state
 const roomMessages = {};
 
 const drone = new Scaledrone(CLIENT_ID);
@@ -13,6 +19,7 @@ drone.on('open', error => {
   if (error) {
     return console.error(error);
   }
+  // get JWT from the Go server for the clientID
   const formData = new FormData();
   formData.append('clientID', drone.clientId);
   fetch('/auth', {body: formData, method: 'POST'})
@@ -29,7 +36,7 @@ drone.on('authenticate', error => {
   joinPersonalRoom();
 });
 
-
+// Start subscribing to messages from the public room
 function joinPublicRoom() {
   const publicRoom = drone.subscribe(PUBLIC_ROOM_NAME);
   publicRoom.on('open', error => {
@@ -39,23 +46,27 @@ function joinPublicRoom() {
     console.log(`Successfully joined room ${PUBLIC_ROOM_NAME}`);
   });
 
+  // Received array of members currently connected to the public room
   publicRoom.on('members', m => {
     members = m;
     me = members.find(m => m.id === drone.clientId);
     DOM.updateMembers();
   });
 
+  // New member joined the public room
   publicRoom.on('member_join', member => {
     members.push(member);
     DOM.updateMembers();
   });
 
+  // Member left public room (closed browser tab)
   publicRoom.on('member_leave', ({id}) => {
     const index = members.findIndex(member => member.id === id);
     members.splice(index, 1);
     DOM.updateMembers();
   });
 
+  // Received public message
   publicRoom.on('message', message => {
     const {data, member} = message;
     if (member && member !== me) {
@@ -67,6 +78,7 @@ function joinPublicRoom() {
   });
 }
 
+// Start subscribing to messages from my private room (PMs to me)
 function joinPersonalRoom() {
   const roomName = createPrivateRoomName(drone.clientId);
   const myRoom = drone.subscribe(roomName);
@@ -136,6 +148,7 @@ const DOM = {
     room: document.querySelector('.room'),
   },
 
+  // Send message to Scaledrone and clear the input
   sendMessage() {
     const {input} = this.elements;
     const value = input.value;
@@ -151,6 +164,7 @@ const DOM = {
     this.addMessageToList(value, me);
   },
 
+  // Create DOM element with member name and color
   createMemberElement(member) {
     const { name, color } = member.authData;
     const el = document.createElement('div');
@@ -158,6 +172,7 @@ const DOM = {
     el.className = 'member';
     el.style.color = color;
     if (member !== me) {
+      // Listen to user clicking on another user
       el.addEventListener('click', () =>
         changeRoom(member.authData.name, createPrivateRoomName(member.id))
       );
@@ -165,6 +180,7 @@ const DOM = {
     return el;
   },
 
+  // Rerender the list of connected members
   updateMembers() {
     this.elements.me.innerHTML = '';
     this.elements.me.appendChild(this.createMemberElement(me));
@@ -174,6 +190,7 @@ const DOM = {
     );
   },
 
+  // Create a DOM element for the message
   createMessageElement(text, member) {
     const el = document.createElement('div');
     el.appendChild(this.createMemberElement(member));
@@ -182,6 +199,7 @@ const DOM = {
     return el;
   },
 
+  // Add message element to the messages container
   addMessageToList(text, member) {
     const el = this.elements.messages;
     const wasTop = el.scrollTop === el.scrollHeight - el.clientHeight;
@@ -199,9 +217,11 @@ const DOM = {
     this.elements.messages.innerHTML = '';
   },
 };
+// Listen to submitting the input form
 DOM.elements.form.addEventListener('submit', () =>
   DOM.sendMessage()
 );
+// Listen to user clicking on the public room label
 DOM.elements.room.addEventListener('click', () =>
   changeRoom('Public room', PUBLIC_ROOM_NAME)
 );
